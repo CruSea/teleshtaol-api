76bb334978d663c7d3a64e9a8e46de4879acee0f
MossJo <mssyonas@gmail.com>
2018-05-01 14:39:11 +0300

    article comment modified

--- app/Http/Controllers/ArticleCommentController.php
+++ app/Http/Controllers/ArticleCommentController.php
@@ -10,12 +10,8 @@ use App\Http\Controllers\Controller;
 
 class ArticleCommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+   
+    public function index($id)
     {
        $comment = Comment::all();
        return response()->json($comment);
